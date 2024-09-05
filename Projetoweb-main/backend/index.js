7622ce05766a05c9166a7840963a2fac7607faeb
Giovanna Pires dos Reis <giovannapires376@gmail.com>
2024-09-05 17:05:00 -0300

    criação do metodo para obter todos os produtos

--- Projetoweb-main/backend/index.js
+++ Projetoweb-main/backend/index.js
@@ -1,7 +1,8 @@
 const express= require('express')
-const cors = requre ('cores')
+const cors = require ('cors')
 const app= express()
 const ClienteRoutes = require ("./routes/ClienteRoutes")
+const ProdutoRoutes = require ("./routes/ProdutoRoutes")
 
 //configuração de resposta
 app.use(express.json())
@@ -9,7 +10,8 @@ app.use(cors({credentials: true, origin:'http://localhost:3000'}))
 
 app.use(express.static('public'))
 
-/* habilitar uso de rotas pelo express */
-app.use('/clientes', ClienteRoutes)
 
-AudioParamMap.listen(5000)
+/* habilitar uso de rotas pelo express */
+app.use('/produtos',ProdutoRoutes)
+app.use('/clientes', ClienteRoutes)
+app.listen(5000)
